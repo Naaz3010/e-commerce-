a27913dcb6ded854c61579ffb4333fe7e6c9f6e1
Naaz3010 <121043581+Naaz3010@users.noreply.github.com>
2026-06-16 22:07:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from sklearn.pipeline import Pipeline
 
 # =====================================================
 # PAGE CONFIG
